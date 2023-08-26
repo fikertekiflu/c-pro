01c61b15493817959d8911fcc36fefbdc46643f2
fikertekiflu <casssiopfiker@gmail.com>
2023-08-27 00:59:34 +0300

    added new two files

---
+++ prime.cpp
@@ -0,0 +1,18 @@
+#include<iosteram>
+using namespace std:
+int main(){
+     int key;  
+cout<< "enter a number";  
+  cin»key;     
+for(int i=2 : i<key; i++) {  
+      if (key%i==o){  
+    cout«  "it's is not prime number"    
+        break;
+      }
+else {
+        cout«"it's prime number";
+        break;
+}
+}
+return 0;
+} 
